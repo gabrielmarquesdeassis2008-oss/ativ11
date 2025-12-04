b364153e802ce66ab47f5606414f9d7f78797e01
gabrielmarquesdeassis2008 <gabrielmarquesdeassis2008@gmail.com>
2025-12-04 10:31:49 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,20 @@
-let i=10
-for(i=10;i<=100;i=i+10){
-    document.writeln(i)
+let nota1=parseInt(prompt('digite sua nota'))
+let nota2=parseInt(prompt('digite sua segunda nota'))
+média=(nota1+nota2)/2
+
+if(média<=4.5){
+    document.writeln('Reprovado'+média)
+}
+else if(média>=4.6 && média<=5.9){
+    document.writeln('Aprovado por média'+ média)
+}
+else if(média>=6 && média<=9.9){
+    document.writeln('Aprovado, acima da média' + média)
+}
+
+else if(média==10){
+    document.writeln('Aprovado com média máxima, Parabéns'+média)
+}
+else {
+    document.writeln('Nota Inválida')
 }
